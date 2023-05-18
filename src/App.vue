65cc0c9e1254748a4c26de5f80ffceb0596d79b5
Gimillo <jaimegimillo@gmail.com>
2023-05-18 20:44:16 +0200

    Added routes

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <main>
-    <HelloWorld msg="Hello world!"/>
-  </main>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <span class="mx-3 mark lead">Chat Babel</span>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="d-flex justify-content-center p-3">
+    <router-view/>
+  </div>
 </template>
-
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
