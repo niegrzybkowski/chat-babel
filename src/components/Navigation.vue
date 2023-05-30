1e31b0de7e89a7578b17ab3e407f246efca92152
Kacper Grzymkowski <kacper.grzymkowski00@gmail.com>
2023-05-30 11:17:49 +0200

    Localize navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,13 +11,13 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="/login">Login</a>
+          <a class="nav-link" href="/login"> {{ current_localization.login }}</a>
         </li>
         <li class="nav-item" v-if="accessToken != ''">
-          <a class="nav-link" href="/rooms">Rooms</a>
+          <a class="nav-link" href="/rooms">{{ current_localization.rooms }}</a>
         </li>
         <li class="nav-item" v-if="accessToken != ''">
-          <a class="nav-link" href="/" @click="logout">Logout</a>
+          <a class="nav-link" href="/" @click="logout">{{ current_localization.logout }}</a>
         </li>
       </ul>
     </div>
@@ -25,10 +25,10 @@
       <strong>{{ username }}</strong>
     </span>
     <select class="form-select me-5" style="width: fit-content;" @change="onLanguageChange($event)" v-model="language">
-      <option value="en">English</option>
-      <option value="pl">Polish</option>
-      <option value="es">Spanish</option>
-      <option value="fr">French</option>
+      <option value="en">English {{current_localization.en_lang}} </option>
+      <option value="pl">polski {{current_localization.pl_lang}} </option>
+      <option value="es">español {{current_localization.es_lang}} </option>
+      <option value="fr">français {{current_localization.fr_lang}} </option>
     </select>
   </nav>
 </template>
@@ -55,7 +55,13 @@
         'accessToken',
         'username',
         'language'
-      ])
+      ]),
+      current_localization() {
+        if (!this.localizations[this.$store.state.language]){
+          return this.localizations[this.default_language]
+        }
+        return this.localizations[this.$store.state.language]
+       }
     }
   }
 </script>
