a52a2ccc03fac5c378a4df2ff0b64e04291ef20c
Kacper Grzymkowski <kacper.grzymkowski00@gmail.com>
2023-06-10 12:55:28 +0200

    a

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -101,7 +101,7 @@
           if (new_messages.length == 0) {
             return
           }
-          this.messages =new_messages.sort((m1, m2) => m1.Time - m2.Time);
+          this.messages = old_messages.concat(new_messages).sort((m1, m2) => m1.Time - m2.Time);
           this.messages.map(message => {
             const old_message = old_messages.filter(old_message => old_message.ID == message.ID);
             if (old_message.length) message['translating'] = old_message[0].translating;
